54d12840e45198123217734ab4b6691eb83e6e9b
wjchk <wanjochan@gmail.com>
2024-04-01 21:27:16 +0800

    for better diff

---
+++ jevalx.js
@@ -0,0 +1,54 @@
+const processWtf = typeof(process)=='undefined'?undefined:process;
+const setTimeoutWtf=setTimeout,PromiseWtf=Promise,ProxyWtf=Proxy,ErrorWtf=Error;
+const PromiseWtf_prototype_then = PromiseWtf.prototype.then;//
+const ObjectWtf = Object;//
+const ReflectWtf = Reflect, FunctionWtf = Function;
+var jevalx = async(js,ctx,timeout=60000,More=['process','eval','require','Reflect','Function'],vm=require('node:vm'))=>{
+  let Wtf={};
+  for(let k of[...Object.keys(globalThis),...More]){Wtf[k]=globalThis[k];delete globalThis[k]}
+
+  //inspired by @j4k0xb:
+  let the_process;
+  try{Object.defineProperty(globalThis,'process',{get(k){ return the_process},set(o){ the_process=o}})}catch(ex){}
+
+  //as sandbox, we just need the js run in ctx, remove as much as we can if vulnerable:
+  delete Promise;delete Error;delete Proxy;delete process;delete Reflect;
+  delete Object.prototype.__proto__;//
+  delete Object.prototype.constructor;//
+  delete Array.prototype.__proto__;//
+  delete Array.prototype.constructor;//
+  delete Function;
+
+  let rst,err,evil=false;
+  try{
+    rst = await new PromiseWtf(async(r,j)=>{
+      try{
+        rst = vm.createScript('delete eval;delete Promise;delete Error;delete Proxy;delete Reflect;delete Function;'+//NOTES: works until new spoil case.
+          js,{importModuleDynamically(specifier, referrer, importAttributes){evil=true;globalThis['process'] = undefined}
+        }).runInContext(vm.createContext(ctx||{}),{breakOnSigint:true,timeout})
+        if (evil){ return j({message:'EvilImport',js}) }
+        if(rst==globalThis) rst = {message:'EvilGlobal',js};
+        let typeof_rst = typeof(rst);
+
+        //inspired by @XmiliaH: .then is vulnerable:
+        if(rst){ delete rst.then }
+
+        if (rst instanceof PromiseWtf) rst = await rst;
+        if ('function'==typeof rst){ rst = {message:'EvilFunction'} }
+      }
+      catch(ex){ err = ex&&ex.message ? {message:ex.message} : {message:'EvilSandbox',js} }
+      if (err) j(err); else r(rst);
+    })
+  }catch(ex){ err = ex }
+  return new PromiseWtf(async(r,j)=>setTimeoutWtf(()=>{
+    Object = ObjectWtf;
+    Function = FunctionWtf;
+    for(var k in Wtf){globalThis[k]=Wtf[k]};
+    Promise=PromiseWtf;Error=ErrorWtf;Proxy=ProxyWtf;
+    Promise.prototype.then = PromiseWtf_prototype_then;//dirty patch. find better way later.
+    if (err) j(err); else r(rst);
+  },1));
+};
+
+if (typeof module!='undefined') module.exports = {jevalx}
+
---
+++ test_jevalx.js
@@ -0,0 +1,263 @@
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('WARNING unhandledRejection', promise, 'reason:', reason);
+});
+
+var {jevalx} = require('./jevalx.js');
+
+(async()=>{
+  console.log('--------- TEST START -----------');
+})().then(async()=>{
+  let code = `async function f() {}
+void (async () => {
+        await f();
+        await f();
+        await f();
+        await f();
+        this.constructor
+.constructor("return process")()
+.mainModule.require("fs")
+.writeFileSync("pwned_case_a", "");
+    })();`;
+  try{
+    console.log('AAAA result=',await jevalx(code));
+  }catch(ex){
+    console.log('AAAA ex=',ex);
+  }
+  console.log('AAAA check=',typeof(process),typeof(Promise));
+}).then(async()=>{
+  var code=`
+    const Symbol= Object.getOwnPropertySymbols(Array)[0].constructor;
+    const customInspectSymbol = Symbol.for({
+      toString: () => "nodejs.util.inspect.custom",
+    });
+    throw {
+      [customInspectSymbol]: () => {
+        this.constructor
+          .constructor("return process")()
+          .mainModule.require("fs")
+          .writeFileSync("pwned_case_b", "");
+      },
+    }
+  `;
+  try{
+    console.log('BBBB result=',await jevalx(code));
+  }catch(ex){
+    console.log('BBBB ex=',typeof ex,ex);
+  }
+  console.log('BBBB check=',typeof(process),typeof(Promise));
+}).then(async()=>{
+//return;//tmp skip
+var code=`
+Object.defineProperty(this.constructor.constructor("return this")(),"process",{set(process) { process.mainModule.require("fs").writeFileSync("pwned_case_c", ""+(typeof this)+this)}})
+`
+  try{
+    console.log('CCCC result=',await jevalx(code));
+  }catch(ex){
+    console.log('CCCC ex=',typeof ex,ex);
+  }
+  console.log('CCCC check=',typeof(process),typeof(Promise));
+}).then(async()=>{
+var code=`
+throw {toString: eval.bind(null, "import('fs').then(m=>m.writeFileSync('pwned_case_d', ''))")};
+`
+  try{
+    console.log('DDDD result=',await jevalx(code));
+  }catch(ex){
+    console.log('DDDD ex=',typeof ex,ex);
+  }
+  console.log('DDDD check=',typeof(process),typeof(Promise));
+
+}).then(async()=>{
+var code=`
+const code = "import('fs').then(m=>m.writeFileSync('pwned_case_e', '')).then(o, r)";
+const funcCtor = import('').catch(_=>_).constructor.constructor;
+const func = funcCtor.bind(null, 'o', 'r', code);
+const obj = {};
+obj.__defineGetter__("then", func);
+//void(async()=>{await obj})();//wont work too
+obj
+`
+  try{
+    console.log('EEEE result=',await jevalx(code));
+  }catch(ex){
+    console.log('EEEE ex=',typeof ex,ex);
+  }
+  console.log('EEEE check=',typeof(process),typeof(Promise));
+
+}).then(async()=>{
+  var code=`
+  ({}).constructor.defineProperty(
+    this.constructor.constructor("return this")(),
+    "_process_or_any",{ set(process) { process.mainModule.require("fs").writeFileSync("pwned_case_f", ""); },
+  }
+)
+  `;
+  try{
+    console.log('FFFF result=',await jevalx(code));
+  }catch(ex){
+    console.log('FFFF ex=',typeof ex,ex);
+  }
+  console.log('FFFF check=',typeof(process),typeof(Promise));
+  
+}).then(async()=>{
+  //var code=`[].constructor.constructor("return Proxy")()`;//anywhere the Proxy?
+  //var code=`let global_ = ({}).constructor.constructor("return this")();global_.Proxy`;
+  var code=`
+  new Proxy((_) => _, {
+  get: new Proxy((_) => _, {
+    apply: function (target, thisArg, args) {
+      args.constructor
+        .constructor("return process")()
+        ?.mainModule.require("fs")
+        .writeFileSync("pwned_case_g", "");
+    },
+  }),
+});`
+  try{
+    console.log('GGGG result=',await jevalx(code));
+  }catch(ex){
+    console.log('GGGG ex=',typeof ex,ex);
+  }
+  console.log('GGGG check=',typeof(process),typeof(Promise));
+}).then(async()=>{
+  var code=`
+  const hostGlobal = this.constructor.constructor("return this")();
+hostGlobal.Promise = function (executor) {
+  return new Promise(executor).then(() => {
+    hostGlobal.process.mainModule.require("fs").writeFileSync("pwned", "");
+  });
+}
+`
+  try{
+    console.log('HHHH result=',await jevalx(code));
+  }catch(ex){
+    console.log('HHHH ex=',typeof ex,ex);
+  }
+  console.log('HHHH check=',typeof(process),typeof(Promise));
+}).then(async()=>{
+var code=`
+const promise = import('').catch(_=>_);
+const oldThen = promise.__proto__.then;
+var global = oldThen.constructor('return this')();
+promise.__proto__.then = function() {
+	if (global.process) {
+		global.process.mainModule.require("fs").writeFileSync("pwned_case_i", "");
+		promise.__proto__.then = oldThen;
+	}
+	return oldThen.apply(this, arguments);
+};
+1
+`
+  try{
+    console.log('IIII result=',await jevalx(code));
+  }catch(ex){
+    console.log('IIII ex=',typeof ex,ex);
+  }
+  console.log('IIII check=',typeof(process),typeof(Promise));
+
+}).then(async()=>{
+var code=`
+eval.bind(null, "import('fs').then(m=>m.writeFileSync('pwned_case_j', ''))")
+`
+  try{
+    console.log('JJJJ result=',await jevalx(code));
+  }catch(ex){
+    console.log('JJJJ ex=',typeof ex,ex);
+  }
+  console.log('JJJJ check=',typeof(process),typeof(Promise));
+}).then(async()=>{
+var code=`
+Reflect.defineProperty(Function.prototype, 'then', {
+	get() {
+		this();
+	}
+});
+`
+  try{
+    console.log('KKKK result=',await jevalx(code));
+  }catch(ex){
+    console.log('KKKK ex=',typeof ex,ex);
+  }
+  console.log('KKKK check=',typeof(process),typeof(Promise));
+}).then(async()=>{
+var code=`
+Function.bind(null, "import('fs').then(m=>m.writeFileSync('pwned', ''))")
+`
+  try{
+    console.log('LLLL result=',await jevalx(code));
+  }catch(ex){
+    console.log('LLLL ex=',typeof ex,ex);
+  }
+  console.log('LLLL check=',typeof(process),typeof(Promise));
+}).then(async()=>{
+var code=`
+const Function = (_=>_).constructor;
+Object.defineProperty(Function.prototype, 'then', { get() { this(); } });
+Function.bind(null, "import('fs').then(m=>m.writeFileSync('pwned_case_m', ''))")
+`
+  try{
+    console.log('MMMM result=',await jevalx(code));
+  }catch(ex){
+    console.log('MMMM ex=',typeof ex,ex);
+  }
+  console.log('MMMM check=',typeof(process),typeof(Promise));
+
+}).then(async()=>{
+var code=`
+this.__proto__.__defineGetter__('',function(){ this.process?.mainModule.require("fs").writeFileSync("pwned_case_n","") })
+`
+  try{
+    console.log('NNNN result=',await jevalx(code));
+  }catch(ex){
+    console.log('NNNN ex=',typeof ex,ex);
+  }
+  console.log('NNNN check=',typeof(process),typeof(Promise));
+}).then(async()=>{
+var code=`
+const Function = (_=>_).constructor;
+const obj = {
+	get then() {
+		Object.defineProperty(this, 'then', {
+			get: Function.bind(null, "import('fs').then(m=>m.writeFileSync('pwned_case_N', ''))")
+		});
+		Object.setPrototypeOf(this, import('').catch(_=>_).constructor);
+	}
+};
+obj
+`
+  try{
+    console.log('OOOO result=',await jevalx(code));
+  }catch(ex){
+    console.log('OOOO ex=',typeof ex,ex);
+  }
+  console.log('OOOO check=',typeof(process),typeof(Promise));
+
+}).then(async()=>{
+var code=`
+this.constructor.prototype.__defineGetter__('',function(){ this.process?.mainModule.require("fs").writeFileSync("pwned_case_O","") })
+`;
+
+  try{
+    console.log('PPPP result=',await jevalx(code));
+  }catch(ex){
+    console.log('PPPP ex=',typeof ex,ex);
+  }
+  console.log('PPPP check=',typeof(process),typeof(Promise));
+
+}).then(async()=>{
+  //basic normal case:
+  console.log('expected x**y==8',await jevalx('x**y',{x:2,y:3}));
+  console.log('expected x**y==81',await jevalx('(async()=>x**y)()',{x:3,y:4}));
+
+  //console.log('check .process',await jevalx('this.constructor.constructor("return this")().process'));
+  console.log('check process',await jevalx('[].constructor.constructor("return typeof(process)")()'));
+
+  //console.log('check "this"',await jevalx('[this,2**3]'));
+  console.log('ZZZ final check=',typeof(process),typeof(Promise));
+  console.log(Promise);
+  console.log(Promise.prototype);
+  console.log(Proxy);
+  console.log(Error);
+  console.log('--------- TEST END -----------');
+});
+
