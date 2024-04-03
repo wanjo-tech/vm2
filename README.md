86afcd2ecf88594f697a705a5f67b6b44f6297a2
wjchk <wanjochan@gmail.com>
2024-04-03 23:04:24 +0800

    cleanups

--- README.md
+++ README.md
@@ -19,8 +19,13 @@ https://github.com/wanjo-tech/vm2/blob/main/test_jevalx.js
 ## key notes
 
 * (DONE) process(and few others) must be hidden before calling, and then restore after all set.
+* (DONE) using setTimeout to wait for the vm done.
 * (DONE) import() must be banned. 
-* (DONE) Evil Promise must be banned
-* (DONE) \_\_proto\_\_ hacks must be defended.
+* (DONE) Evil Fake Promise must be banned.
+* (DONE) hacks (\_\_proto\_\_ , __defineGetter__) must be defended.
 
 thanks to @j4k0xb and @XmiliaH keep sending wonderful cases even being annoyed by me ;)
+
+## application
+
+I use it in my kk project.
--- jevalx.js
+++ jevalx.js
@@ -1,12 +1,8 @@
-//sandbox: we just need the js run in ctx, remove thinks if vulnerable.
-
-const processWtf = typeof(process)=='undefined'?undefined:process;
-const setTimeoutWtf=setTimeout,PromiseWtf=Promise;
+const processWtf = require('process');
+const setTimeoutWtf=setTimeout;
+const PromiseWtf=Promise;
 const Object_keys = Object.keys;
 const Object_getPrototypeOf = Object.getPrototypeOf;
-//const Object_defineProperty = Object.defineProperty;
-//const PromiseWtf_prototype_then = PromiseWtf.prototype.then;//for dirty patch
-
 const prejs_delete = [
   'eval','process',
   'Object.getPrototypeOf','Object.defineProperties','Object.defineProperty','Object.getOwnPropertySymbols',
@@ -16,7 +12,6 @@ var jevalx_core = async(js,ctx,timeout=60000,timeout_race=666,vm=require('node:v
   let rst,err,evil=false,done=false;
   let tmpHandler = (reason, promise)=>{ err = {message:''+reason,js}};
   processWtf.addListener('unhandledRejection',tmpHandler);
-
   let Wtf={};
   try{
     for(let k of[...Object_keys(globalThis),...['process','require']]){if(globalThis[k]){Wtf[k]=globalThis[k]};delete globalThis[k]};
@@ -26,7 +21,7 @@ var jevalx_core = async(js,ctx,timeout=60000,timeout_race=666,vm=require('node:v
         rst = vm.createScript(prejs_delete+'\n'
           +js,{importModuleDynamically(specifier, referrer, importAttributes){
             evil=true; err = {message:'EvilImport',js};
-            globalThis['process'] = undefined;//very important! NOT delete.
+            globalThis['process'] = undefined;//very important! NOT delete..
         }}).runInContext(vm.createContext(ctx||{}),{breakOnSigint:true,timeout});
         if(rst==globalThis) throw {message:'EvilGlobal',js};
         for (var i=0;i<9;i++) {
@@ -38,7 +33,6 @@ var jevalx_core = async(js,ctx,timeout=60000,timeout_race=666,vm=require('node:v
             rst = rst();
           }else if (rst.then){
             if (rst instanceof PromiseWtf){//host Promise
-              //rst = await rst;
               throw {message:'EvilPromiseHost',js}
             }else if ((''+rst)=='[object Promise]'){//sandbox Promise. dirty, will improve later...
               rst = await new PromiseWtf(async(r,j)=>{
@@ -60,30 +54,5 @@ var jevalx_core = async(js,ctx,timeout=60000,timeout_race=666,vm=require('node:v
   if (evil || err) { throw err }
   return rst;
 }
-
 var jevalx = jevalx_core;
-
-//Stronger dose!!! Once jevalx_core passed all test, this one is no longer needed.
-//const ProxyWtf=Proxy,ErrorWtf=Error,ObjectWtf = Object,ReflectWtf = Reflect, FunctionWtf = Function;
-//var jevalx = async(js,ctx,timeout=60000,More=['process','eval','require','Reflect','Function','Proxy'])=>{
-//  //TODO seems no need any more
-//  let the_process;
-//  try{Object.defineProperty(globalThis,'process',{get(k){ return the_process},set(o){ the_process=o}})}catch(ex){}
-//
-//  let rst,err;
-//  try{
-//    rst = await jevalx_core(js,ctx,timeout);
-//  }catch(ex){ err = ex&&ex.message ? ex : {message:'EvilSandbox',js} }
-//  return new PromiseWtf((r,j)=>setTimeoutWtf(async()=>{
-//    //Object = ObjectWtf;
-//    //Function = FunctionWtf;
-//    //for(var k in Wtf){globalThis[k]=Wtf[k]};
-//    //Promise=PromiseWtf;Error=ErrorWtf;Proxy=ProxyWtf;
-//    //Promise.prototype.then = PromiseWtf_prototype_then;//dirty patch. find better way later.
-//    //process = processWtf;
-//    if (err) j(err); else r(rst);
-//  },1));
-//};
-
 if (typeof module!='undefined') module.exports = {jevalx,jevalx_core}
-
