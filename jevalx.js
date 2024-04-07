60d8f8dd61ac9f3faacf79033637aa9465178f26
wjchk <wanjochan@gmail.com>
2024-04-07 20:11:32 +0800

    passed

--- jevalx.js
+++ jevalx.js
@@ -18,20 +18,24 @@ function findEvilGetter(obj,deep=3) {
   }
   return false;
 }
-let jevalx_raw = (js,ctxx,timeout=666,js_opts)=>[ctxx,vm.createScript(js,js_opts).runInContext(ctxx,{breakOnSigint:true,timeout})];
+
+//e.g. [ctxx,rst] = await jevalx_raw('Math.random()')
+// [ctxx,rst] = await jevalx_raw('[({}).constructor,constructor]') //sandbox and host
+let jevalx_raw = (js,ctxx,timeout=666,js_opts)=>(ctxx=ctxx||vm.createContext(new function ObjectX(){}),[ctxx,vm.createScript(js,js_opts).runInContext(ctxx,{breakOnSigint:true,timeout})]);
 
 const sFunction="(...args)=>eval(`(${args.slice(0,-1).join(',')})=>{${args[args.length-1]}}`)";
 
 const jevalx_ext = (js,ctx,timeout=666,js_opts)=>{
   let rst,ctxx;
   if (!vm.isContext(ctx||{})) {
-    ctxx = vm.createContext(new(function Object(){})/*,{microtaskMode:'afterEvaluate'}*/);
+    [ctxx,rst] = jevalx_raw(`delete Function;constructor.__proto__.constructor=Object.__proto__.constructor=Function=${sFunction};Object.freeze(constructor,__proto__.constructor);for(let k of Object.getOwnPropertyNames(Object))delete Object[k];delete Object.prototype.__defineGetter__;delete Object.prototype.__defineGetter__;`);
     //very important: replace dangerous
     ctxx.eval=(js)=>jevalx_raw(js,ctxx,timeout,js_opts)[1];//NOTES no need use js_opts for eval()
     ctxx.Symbol = (...args)=>{throw {message:'TodoSymbol'}};
     ctxx.Reflect=(...args)=>{throw {message:'TodoReflect'}};
     ctxx.Proxy=(...args)=>{throw {message:'TodoProxy'}};
-    [ctxx,rst] = jevalx_raw(`Function=${sFunction};constructor.__proto__.constructor=Function;Object=constructor.__proto__`,ctxx);
+    ////[ctxx,rst] = jevalx_raw(`Function=${sFunction};constructor.__proto__.constructor=Function;Object=constructor.__proto__`,ctxx);
+    //[ctxx,rst] = jevalx_raw(`constructor.__proto__.constructor=Object.__proto__.constructor=Function=${sFunction};`,ctxx);
     if (ctx) Object_assign(ctxx,ctx);
   }else{ ctxx = ctx; }
   return jevalx_raw(js,ctxx,timeout,js_opts)
@@ -80,4 +84,63 @@ let jevalx_core = async(js,ctx,timeout=666)=>{
 }
 var jevalx = jevalx_core;
 if (typeof module!='undefined') module.exports = {jevalx,jevalx_core,jevalx_raw,jevalx_ext}
+
+/**
+/////////////////// HOST:
+
+typeof                                        // inspect                    // toString()
+            ////////////////////////////////
+'undefined' undefined                         //  undefined                 // 'undefined'
+            ////////////////////////////////  i.e. Object.getPrototypeOf(Object.create(null))
+'object'    null                              //null                // 'null'
+            Object.prototype.__proto__
+            ////////////////////////////////  TO REMOVE: __defineGetter__, __defineGetter__
+'object'    Object.prototype                  //[Object: null prototype] {}// [object Object]
+            Object.__proto__.__proto__ 
+            (new Object()).__proto__          
+            ({}).__proto__                    
+            Function.prototype.__proto__      
+            ////////////////////////////////
+            {}                                // {}                        // [object Object]
+            ////////////////////////////////
+'function'  Function.prototype                // {}                        // 'function () { [native code] }'
+            Object.__proto__
+            Function.__proto__
+            Array.__proto__
+            ////////////////////////////////
+'function'  Object.__proto__.constructor      // [Function: Function]      // 'function Function() { [native code] }'
+            (_=>_).constructor
+            (function(){}).constructor
+            [].__proto__.push.constructor
+            Function
+            ////////////////////////////////
+'function'  ([]).constructor                  // [Function: Array]         //'function Array() { [native code] }'
+            Array
+            ////////////////////////////////  defineProperties,defineProperty,getPrototypeOf,getOwnPropertySymbols,assign,freeze,keys;
+'function'  ({}).constructor                  // [Function Object]         //'function Object() { [native code] }'
+            Object                             * sandbox
+            constructor                        * sandbox and host
+            this.constructor                   * sandbox and host
+            globalThis.constructor             * sandbox and host
+            global.constructor                 * only in host
+            ////////////////////////////////
+'object'    this                               * sandbox
+            globalThis                         * sandbox
+            global                             * only in host
+            ////////////////////////////////
+'object'    ([]).__proto__                    // Object(0)[]               // ''
+            Array.prototype
+            ////////////////////////////////  host Function (danger..)
+            constructor.__proto__.constructor
+            constructor.constructor
+            //////////////////////////////// //Object.create(null)
+Object.getPrototypeOf(Object.create(null)) === null; // true
+'toString' in Object.create(null); // false
+'hasOwnProperty' in Object.create(null); // false
+let obj1 = Object.create(null);
+let obj2 = Object.create(null);
+console.log(obj1 === obj1); // true
+console.log(obj1 === obj2); // false
+console.log(obj1 == obj2);  // false
+*/
 
--- test_loop_check_sandbox.js
+++ test_loop_check_sandbox.js
@@ -2,23 +2,13 @@
   const vm = require('node:vm');
   var {jevalx_raw,jevalx_ext} = require('./jevalx');
   let ctxx,rst;
-  ctxx = vm.createContext(new(function Object(){}));
-  ctxx.eval=(js)=>jevalx_raw(js,ctxx,timeout,js_opts)[1];//NOTES no need use js_opts for eval()
-  ctxx.Symbol = (...args)=>{throw {message:'TodoSymbol'}};
-  ctxx.Reflect=(...args)=>{throw {message:'TodoReflect'}};
-  ctxx.Proxy=(...args)=>{throw {message:'TodoProxy'}};
-
-  //const sFunction="(...args)=>eval(`(${args.slice(0,-1).join(',')})=>{${args[args.length-1]}}`)";
-  //[ctxx,rst] = jevalx_raw(`Function=${sFunction};constructor.__proto__.constructor=Function;Object=constructor.__proto__`,ctxx);
-
-  [ctxx,rst] = jevalx_ext(`0;`,ctxx);
   let dump=async(v)=>{
+    let code = `(()=>{let v=${v};return[typeof v,''+v]})()`;
     try{
-      let vv = await jevalx_raw(`(()=>{let v=${v};return[typeof v,''+v]})()`,ctxx)[1];
-      let ss = JSON.stringify(vv)
-      console.log(v,'=>',ss);
+      [ctxx,rst] = await jevalx_ext(code,ctxx);
+      console.log(code,'=>',rst);
     }catch(ex){
-      console.error(v,'=>'+ex)
+      console.error(code,'=>'+ex)
     }
   };
   await dump('Object.getPrototypeOf');
@@ -32,78 +22,3 @@
   await dump('[].constructor.prototype');
 })()
 
-/* 
-/////////////////// HOST:
-
-typeof                                        // inspect                    // toString()
-            ////////////////////////////////
-'undefined' undefined                         //  undefined                 // 'undefined'
-            ////////////////////////////////  i.e. Object.getPrototypeOf(Object.create(null))
-'object'    null                              //null                // 'null'
-            Object.prototype.__proto__
-            ////////////////////////////////  TO REMOVE: __defineGetter__, __defineGetter__
-'object'    Object.prototype                  //[Object: null prototype] {}// [object Object]
-            Object.__proto__.__proto__ 
-            (new Object()).__proto__          
-            ({}).__proto__                    
-            Function.prototype.__proto__      
-            ////////////////////////////////
-            {}                                // {}                        // [object Object]
-            ////////////////////////////////
-'function'  Function.prototype                // {}                        // 'function () { [native code] }'
-            Object.__proto__
-            Function.__proto__
-            Array.__proto__
-            ////////////////////////////////
-'object'    ([]).__proto__                    // Object(0)[]               // ''
-            Array.prototype
-            ////////////////////////////////
-'function'  Object.__proto__.constructor      // [Function: Function]      // 'function Function() { [native code] }'
-            Function
-            ////////////////////////////////
-'function'  ([]).constructor                  // [Function: Array]         //'function Array() { [native code] }'
-            Array
-            ////////////////////////////////  defineProperties,defineProperty,getPrototypeOf,getOwnPropertySymbols,assign,freeze,keys;
-'function'  ({}).constructor                  // [Function Object]         //'function Object() { [native code] }'
-            Object
-            constructor                       /* When
-            this.constructor                   * Host
-            globalThis.constructor             * Global
-            global.constructor                 */
-            ////////////////////////////////
-//Object.create(null)
-Object.getPrototypeOf(Object.create(null)) === null; // true
-'toString' in Object.create(null); // false
-'hasOwnProperty' in Object.create(null); // false
-let obj1 = Object.create(null);
-let obj2 = Object.create(null);
-console.log(obj1 === obj1); // true
-console.log(obj1 === obj2); // false
-console.log(obj1 == obj2);  // false
-             ////////////////////////////////
-> ''+Object.getPrototypeOf(new(function(){})())
-'[object Object]'
-
-/////////////////// SANDBOX raw 
-> await jevalx_raw(`''+constructor`,vm.createContext())
-[ {}, 'function Object() { [native code] }' ]
-> await jevalx_raw(`''+constructor`,vm.createContext({}))
-[ {}, 'function Object() { [native code] }' ]
-
-> await jevalx_raw(`''+constructor`,vm.createContext(new(function(){})))
-[ {}, 'function(){}' ]
-> await jevalx_raw(`''+constructor`,vm.createContext(new(function ObjectX(){})))
-[ ObjectX {}, 'function ObjectX(){}' ]
-
-> await jevalx_raw(`''+constructor.__proto__`,vm.createContext(new(function ObjectX(){})))
-[ ObjectX {}, 'function () { [native code] }' ]
-> await jevalx_raw(`''+constructor.__proto__,''+constructor.__proto__.__defineGetter__`,vm.createContext(Object.create(null)))
-[
-  [Object: null prototype] {},
-  'function __defineGetter__() { [native code] }'
-]
-
-/////////////////// SANDBOX 
-
-
-*/
