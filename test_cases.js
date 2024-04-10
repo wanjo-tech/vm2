6bdbde4aed1ac30ff912b1caa97332fea1b810d9
wjchk <wanjochan@gmail.com>
2024-04-10 14:50:53 +0800

    test case r4

--- test_cases.js
+++ test_cases.js
@@ -1,8 +1,7 @@
 let jevalxModule = require('./jevalx.js');
 let assertWtf = require('assert');
 
-//let jevalx = jevalxModule.jevalx_core;
-let jevalx = jevalxModule.jevalx_dev;
+let jevalx = jevalxModule.jevalx;
 
 /*
 (async()=>{ await 1;let u = false; function t(o, e) { import('').then(_=>_, _=>_).then.constructor('return process')()?.mainModule.require("fs").writeFileSync("pwned_case_q7",""); u = true; o(this); } const obj = {__proto__: { get then(){ if (u) { u = false; return undefined; } return t; } }}; return obj })
@@ -43,4 +42,18 @@ q7:async()=>{
        }
      console.log('Q7 check=',typeof(process),typeof(Promise));
    },
+  r4:async()=>{
+var case_id= 'r4';
+var code=`
+import('').catch(_=>_).constructor.__proto__ = {
+        set constructor(f) {f("return process")().mainModule.require("fs").writeFileSync("pwned_${case_id}", "");}
+}
+`;
+       try{
+         console.log(`${case_id} result=`,await jevalx(code,{console_log:console.log}));
+       }catch(ex){
+         console.log(`$case_id} ex=`,ex);
+       }
+     console.log(`${case_id} check=`,typeof(process),typeof(Promise));
+  },
 }
