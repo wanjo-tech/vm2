f282bf6856fc1ae095726f99c3d17f4467bdd332
wjchk <wanjochan@gmail.com>
2024-04-11 10:54:31 +0800

    dumptree add version of nodejs

--- dumptree.bat
+++ dumptree.bat
@@ -1,3 +1,9 @@
-node dumptree.js > dumptree.json
-node dumptree_sandbox.js > dumptree_sandbox.json
+@rem node dumptree.js > dumptree.json
+
+node -e "const child = require('child_process').spawn('node', ['dumptree', '>', 'dumptree'+process.version+'.json'],{shell:true}); child.stdout.on('data', data => console.log('' + data)); child.stderr.on('data', data => console.error('STDERR'+data));"
+
+@rem node dumptree_sandbox.js > dumptree_sandbox.json
+node -e "const child = require('child_process').spawn('node', ['dumptree_sandbox', '>', 'dumptree_sandbox'+process.version+'.json'],{shell:true}); child.stdout.on('data', data => console.log('' + data)); child.stderr.on('data', data => console.error('STDERR'+data));"
+
 @rem node dumptree_sandbox_dev.js > dumptree_sandbox_dev.json
+
