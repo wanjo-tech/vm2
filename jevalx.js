0351097f6d25c58ee988daa7188407add2bc7826
wjchk <wanjochan@gmail.com>
2024-04-13 22:26:17 +0800

    support ctx as session now

--- jevalx.js
+++ jevalx.js
@@ -6,29 +6,28 @@ const console_log = console.log;
 const Object_getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
 const Object_getPrototypeOf = Object.getPrototypeOf;
 const Object_getOwnPropertyNames = Object.getOwnPropertyNames;
-const getOwnPropertyNames = Object_getOwnPropertyNames;
 const Object_assign = Object.assign;
 
 const delay = (t,rt)=>new Promise((r,j)=>setTimeout(()=>r(rt),t));
 
 // for __proto__ Pollultion:
-function findEvil(obj,maxdepth=3) {
-  let currentObj = obj;
-  let depth = 0;
-  while (currentObj !== null && currentObj!==undefined && depth < maxdepth) {
-    //'constructor','toString','__proto__',
-    const properties = ['constructor','then'];//Object_getOwnPropertyNames(currentObj);
-    for (let i = 0; i < properties.length; i++) {
-      const descriptor = Object_getOwnPropertyDescriptor(currentObj, properties[i]);
-      if (descriptor && (typeof descriptor.get === 'function' || typeof descriptor.set == 'function')) {
-        return true;
-      }
-    }
-    currentObj = Object_getPrototypeOf(currentObj);
-    depth++;
-  }
-  return false;
-}
+//function findEvil(obj,maxdepth=3) {
+//  let currentObj = obj;
+//  let depth = 0;
+//  while (currentObj !== null && currentObj!==undefined && depth < maxdepth) {
+//    //'constructor','toString','__proto__',
+//    const properties = ['constructor','then'];//Object_getOwnPropertyNames(currentObj);
+//    for (let i = 0; i < properties.length; i++) {
+//      const descriptor = Object_getOwnPropertyDescriptor(currentObj, properties[i]);
+//      if (descriptor && (typeof descriptor.get === 'function' || typeof descriptor.set == 'function')) {
+//        return true;
+//      }
+//    }
+//    currentObj = Object_getPrototypeOf(currentObj);
+//    depth++;
+//  }
+//  return false;
+//}
 
 // for Promise Pollultion:
 const Promise___proto__ = Promise.__proto__;
@@ -45,18 +44,32 @@ let jevalx_raw = (js,ctxx,timeout=666,js_opts)=>[ctxx,vm.createScript(js,js_opts
 
 //const S_FUNCTION = "(...args)=>eval(`(${args.slice(0,-1).join(',')})=>{${args[args.length-1]}}`)";
 
+// X LIKE VOID
 function X(){ if (this instanceof X){ }else{ return new X() } }
 X.prototype.constructor = X;
 Object.setPrototypeOf(X.prototype,null);
 Object.freeze(X.prototype);
-Object.setPrototypeOf(X.__proto__,null);
-Object.setPrototypeOf(X,null);
-Object.freeze(X);
+//if (X.__proto__) Object.setPrototypeOf(X.__proto__,null);//L1
+Object.setPrototypeOf(X,null);//L0
+Object.freeze(X);//L0
+
+function dump(obj) {
+    let props = [];
+    let currentObj = obj;
+    do {
+        props = props.concat(Object_getOwnPropertyNames(currentObj));
+    } while ((currentObj = Object.getPrototypeOf(currentObj)));
+
+    return props.sort().filter(function(e, i, arr) { 
+       if (e!=arr[i+1] && typeof obj[e] == 'function') return true;
+    });
+}
 
 const S_SETUP = [
-  'console','Symbol','Reflect','Proxy','Object.prototype.__defineGetter__','Object.prototype.__defineSetter__'
+'console',//it is not working
+'Symbol','Reflect','Proxy','Object.prototype.__defineGetter__','Object.prototype.__defineSetter__',
 ].map(v=>'delete '+v+';').join('')
-//IMPORTANT: still need to lock the danger functions of 'this'
+//IMPORTANT: need to lock the danger functions of 'this'
 +[
 //  '__defineGetter__',
 //  '__defineSetter__',
@@ -71,23 +84,9 @@ const S_SETUP = [
 ].map(v=>'Object.setPrototypeOf('+v+',null);Object.freeze('+v+');delete constructor.'+v+';').join('')
 +`
 for(let k of Object.getOwnPropertyNames(Object)){if(['name','fromEntries','keys','entries','is','values','getOwnPropertyNames'].indexOf(k)<0){delete Object[k]}}
-//for(let k of Object.getOwnPropertyNames(Object)){if(['name','fromEntries','keys','entries','is','values','getOwnPropertyNames','getPrototypeOf'].indexOf(k)<0){delete Object[k]}}
 
 //TOOLS
 //AsyncFunction = (async()=>{}).constuctor;
-//for debug:
-//function getAllPrototypeMethods(obj) {
-//    let props = [];
-//    let currentObj = obj;
-//    do {
-//        props = props.concat(Object.getOwnPropertyNames(currentObj));
-//    } while ((currentObj = Object.getPrototypeOf(currentObj)));
-//
-//    return props.sort().filter(function(e, i, arr) { 
-//       if (e!=arr[i+1] && typeof obj[e] == 'function') return true;
-//    });
-//}
-//console.log(getAllPrototypeMethods(constructor));
 
 Promise
 `;
@@ -104,14 +103,14 @@ let sandbox_safe_method = function(m,do_return=false){
 let jevalx_core = async(js,ctx,timeout=666,json_output=false,return_ctx=false,user_import_handler=undefined)=>{
   let ctxx,rst,err,evil=0,jss= JSON.stringify(js);
   let last_resolve,last_reject;//for quicker return.
-    let tmpHandlerReject = (ex, promise)=>{ if (!err) err={message:'EvilXb',js};
-      //err.message!='EvilXb' &&
-      console.log('EvilXb=>',ex,'<=',jss)
-      if (last_reject) last_reject(err);
-    };
     let tmpHandlerException = (ex, promise)=>{ if (!err) err={message:'EvilXa',js};
       //err.message!='EvilXa' &&
       console.log('EvilXa=>',ex,'<=',jss)
+      if (last_reject) last_reject(err);
+    };
+    let tmpHandlerReject = (ex, promise)=>{ if (!err) err={message:'EvilXb',js};
+      //err.message!='EvilXb' &&
+      console.log('EvilXb=>',ex,'<=',jss)
       if (last_reject) last_reject(err);
     };
   try{
@@ -132,14 +131,17 @@ let jevalx_core = async(js,ctx,timeout=666,json_output=false,return_ctx=false,us
       setTimeout(()=>{j({message:'TimeoutX',js,js_opts})},timeout+666)//FOR DEV TEST...
       try{
         //GENESIS
-        //ctxx = vm.createContext(new function(){});//OLD BIGBANG
-        ctxx = vm.createContext(new X);//BIGBANG
-        [ctxx,_Promise] = jevalx_raw(S_SETUP,ctxx);//INIT
-        //ctxx.console = {log:console.log,props:getOwnPropertyNames};//DEV
-        let console_dev = Object.create(null);
-        console_dev['log']=sandbox_safe_method(console.log);
-        ctxx.console = console_dev;
-        if (ctx) Object_assign(ctxx,ctx);//CTX: TODO, need to protect the outer stuff for the __proto__ pollution.
+        if (ctx && vm.isContext(ctx)) { ctxx = ctx } //enable session...
+        else {
+          //ctxx = vm.createContext(new function(){});//OLD BIGBANG
+          ctxx = vm.createContext(new X);//BIGBANG FROM X
+          [ctxx,_Promise] = jevalx_raw(S_SETUP,ctxx);
+          let console_dev = Object.create(null);
+          console_dev['log']=sandbox_safe_method(console.log);
+          ctxx.console = console_dev;
+          //ctxx.console = {log:console.log,props:Object.getOwnPropertyNames,dump};//FOR QUICK DEV
+          if (ctx) Object_assign(ctxx,ctx);//CTX: TODO! need to protect the outer stuff for the __proto__ pollution.
+        }
 
         //PRECAUTION
         ////Host Promise
@@ -154,15 +156,14 @@ let jevalx_core = async(js,ctx,timeout=666,json_output=false,return_ctx=false,us
         Object.freeze(Promise.prototype);//L0 for the import("").catch()
 
         //SIMULATION{{{
-        [ctxx,rst] = jevalx_raw(`(async()=>{let rst=(()=>eval(${jss}))();while(rst){if(rst instanceof Promise){rst=await rst}else if(typeof rst=='function'){rst=rst()}else break}return ${!!json_output}?JSON.stringify(rst):rst})()`,ctxx,timeout,js_opts);
+        [ctxx,rst] = jevalx_raw(`(async()=>{try{let rst=(()=>eval(${jss}))();while(rst){if(rst instanceof Promise){rst=await rst}else if(typeof rst=='function'){rst=rst()}else break}return ${!!json_output}?JSON.stringify(rst):rst}catch(ex){}})()`,ctxx,timeout,js_opts);
         //SIMULATION}}}
 
         //HOUSEWEEP
         rst = await rst;
         if (rst) {
-          //Object.setPrototypeOf(rst,null);//no need anymore? TODO
-          if (findEvil(rst)) throw {message:'EvilProtoX',js};
-          delete rst['toString']; delete rst['constructor']; delete rst['toJSON'];//...maybe no need anymore? TODO
+          //if (findEvil(rst)) throw {message:'EvilProtoX',js};//seem sno need now.
+          delete rst['toString']; delete rst['constructor']; delete rst['toJSON'];//not sure, TODO?
         }
       }catch(ex){ err={message:typeof(ex)=='string'?ex:(ex?.message|| 'EvilXc'),js};
         //err.message=='EvilXc' &&
