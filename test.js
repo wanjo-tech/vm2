40f6980173f404d951ed5432038871fbf7495d13
wjchk <wanjochan@gmail.com>
2024-04-14 22:13:18 +0800

    test case r15c for delay()

--- test.js
+++ test.js
@@ -1244,6 +1244,16 @@ Promise.delay(1000).then(_=>import('').catch(_=>_).constructor.constructor('retu
      }
      console.log(`${case_id} check=`,typeof(process),typeof(Promise),Promise);
   },
+  r15c:async function(){ let case_id = arguments.callee.name; var code=`
+Promise.delay(1234).catch(_=>_).then(_=>import('').catch(_=>_).constructor.constructor('return process')().mainModule.require("fs").writeFileSync("pwned_r15b", ""));Promise.delay(888)
+`;
+     try{
+       console.log(`${case_id} result(raw)=`,await jevalx(code,{},timeout=666,json_output=false));
+     }catch(ex){
+       console.log(`${case_id} ex=`,ex);
+     }
+     console.log(`${case_id} check=`,typeof(process),typeof(Promise),Promise);
+  },
   r16:async function(){ let case_id = arguments.callee.name; var code=`
 const i = import('');
 i.catch.__proto__.call = [].reduce.bind([1,2], Function.call.bind(Function.call), Function.apply.bind(Function, null, ["import('fs').then(m=>m.writeFileSync('pwned_r16', ''))"]));
@@ -1507,5 +1517,6 @@ old helper.
     //  throw('EvilImport');
     //}});
 
+await jevalx(`dumptree(this)`,{dumptree:require('./dumptree')})
 
 */
