1f391f9ece235ed212fc83dd042269dd0d836e57
wjchk <wanjochan@gmail.com>
2024-04-18 16:42:16 +0800

    purify the sandbox;

--- jevalx.js
+++ jevalx.js
@@ -1,25 +1,13 @@
-/** Quick Notes:
-
-All known escape instances are related to the Host Object.
-
-Our strategy involves cutting off connections to the components to mitigate risks.
-
-Keypoints marked '//L0' in the source code.
-
-*/
-
-//L0
 Object.defineProperty(Object.prototype, '__proto__', {
   get() { console.log('host__proto__911_get');return undefined; },
   set(newValue) { console.log('host__proto__911_set',newValue) }
 });
 const X=function(){}
-Object.setPrototypeOf(X.prototype,null);//L0
-Object.setPrototypeOf(X,X.prototype);//L0
+Object.setPrototypeOf(X.prototype,null);
+Object.setPrototypeOf(X,X.prototype);
 
-Object.defineProperty(globalThis,'AsyncFunction',{value:(async()=>{}).constructor,writable:false,enumerable:false,configurable:false});// TOOL
+Object.defineProperty(globalThis,'AsyncFunction',{value:(async()=>{}).constructor,writable:false,enumerable:false,configurable:false});
 
-//@r28
 eval(['Object.prototype.__defineGetter__','Object.prototype.__defineSetter__'].map(v=>'delete '+v+';').join(''));
 
 const Object_getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
@@ -49,16 +37,17 @@ const delay = (t,rt)=>new Promise((r,j)=>setTimeout(()=>r(rt),t));
 let jevalx_raw = (js,ctxx,timeout=666,js_opts)=>[ctxx,vm.createScript(js,js_opts).runInContext(ctxx,{breakOnSigint:true,timeout})];
 
 const S_SETUP = `(()=>{
+let is_sandbox = 'function'!=typeof setTimeout;
 let Object_defineProperty = Object.defineProperty;
-Object_defineProperty(Object.prototype,'__proto__',{
-  get() { console.log('box__proto__911_get');return undefined; },
-  set(newValue) { console.log('box__proto__911_set',newValue) }
-});//L0
+Object_defineProperty(Object.prototype,'__proto__',{get(){},set(newValue){}});//L0
+if (is_sandbox){
+  let WhiteList = ['Object','Array','JSON','Promise','Function','eval','globalThis','Date','Math'];
+  for (let v of Object.getOwnPropertyNames(this)){
+    if (WhiteList.indexOf(v)<0) delete this[v];
+  };
+};
 `+[
-//'console',//L2
-'WebAssembly','Error','AggregateError','EvalError','RangeError','ReferenceError','SyntaxError','TypeError','URIError',//L1
-'Symbol','Reflect','Proxy','Object.prototype.__defineGetter__','Object.prototype.__defineSetter__','Object.prototype.__lookupSetter__','Object.prototype.__lookupGetter__',//L0
-'FinalizationRegistry',//@r29
+'Object.prototype.__defineGetter__','Object.prototype.__defineSetter__','Object.prototype.__lookupSetter__','Object.prototype.__lookupGetter__',//L0
 ].map(v=>'delete '+v+';').join('')
 +`
 Object.defineProperty(globalThis,'AsyncFunction',{value:(async()=>{}).constructor,writable:false,enumerable:false,configurable:false});
@@ -69,9 +58,9 @@ return Promise})()`;
 let jevalx_host_a = [ RangeError, TypeError, Promise, Object, AsyncFunction, Function ];
 let jevalx_core = async(js,ctx,options={})=>{
   let {timeout=666,json_output=false,return_ctx=false,user_import_handler=undefined}=(typeof options=='object'?options:{});
-  if (typeof options=='number') timeout = options;//
+  if (typeof options=='number') timeout = options;
   let ctxx,rst,err,evil=0,jss= JSON.stringify(js),done=false;
-  let last_resolve,last_reject;//for quicker return.
+  let last_reject;
     let tmpHandler = (ex, promise)=>{ if (!err) err={message:ex?.message||'EvilXa',js,code:ex?.code,tag:Promise?'Xb':'Xa',ex};
       if (!done && last_reject) {
         last_reject(err);
@@ -81,23 +70,21 @@ let jevalx_core = async(js,ctx,options={})=>{
     processWtf.addListener('unhandledRejection',tmpHandler);
     processWtf.addListener('uncaughtException',tmpHandler)
     let _Promise;
-    //to support the user_import_handler()
-    let js_opts;//TODO for import()
     await new Promise(async(r,j)=>{
       last_resolve = r, last_reject = j;
-      delay(timeout+666).then(()=>{done=true;j({message:'TimeoutX',js,js_opts})})//L0 @Q7x
+      delay(timeout+666).then(()=>{done=true;j({message:'TimeoutX',js})})//L0 @Q7x
       try{
-        if (ctx && vm.isContext(ctx)) { ctxx = ctx } //SESSION
+        if (ctx && vm.isContext(ctx)) ctxx = ctx;
         else {
-          ctxx = vm.createContext(new X);//BIGBANG
+          ctxx = vm.createContext(new X);
           [ctxx,_Promise] = jevalx_raw(S_SETUP,ctxx);
           _Promise.delay = (t,r)=>new _Promise((rr,jj)=>delay(t).then(()=>(done||rr(r))));
           ctxx.console = {log:console.log};//can be replaced by ctx.console
           if (ctx) Object.assign(ctxx,ctx);
         }
         jevalx_host_a.forEach(o=>(o.prototype.constructor=X));
-        [ctxx,rst] = jevalx_raw(`(async()=>{try{return await(async z=>{while(z&&((z instanceof Promise)&&(z=await z)||(typeof z=='function')&&(z=z())));return ${!!json_output}?JSON.stringify(z):z})(eval(${jss}))}catch(ex){return Promise.reject(ex)}})()`,ctxx,timeout,js_opts);
-        rst = await rst;//above we use (async()=>{...})() which sure is a Promise
+        [ctxx,rst] = jevalx_raw(`(async()=>{try{return await(async z=>{while(z&&((z instanceof Promise)&&(z=await z)||(typeof z=='function')&&(z=z())));return ${!!json_output}?JSON.stringify(z):z})(eval(${jss}))}catch(ex){return Promise.reject(ex)}})()`,ctxx,timeout);
+        rst = await rst;
         done = true;
         if (findEvil(rst)) throw {message:'EvilProtoX',js};//@r4
         if (rst) {delete rst.then;delete rst.toString;delete rst.toJSON}//@Q15
@@ -124,16 +111,16 @@ let jevalx_core = async(js,ctx,options={})=>{
     if (err?.code=='ERR_VM_DYNAMIC_IMPORT_CALLBACK_MISSING_FLAG') { err.message = 'EvilImportX'; err.code='EVIL_IMPORT_FLAG';}
     if (err?.code=='ERR_VM_DYNAMIC_IMPORT_CALLBACK_MISSING') { err.message = 'EvilImport'; err.code='EVIL_IMPORT';}
     if (err?.code=='ERR_SCRIPT_EXECUTION_TIMEOUT') {err.message = 'Timeout'+timeout;err.code='TIMEOUT';}
-    if (!err?.time) err.time = new Date().getTime();//for app
-    if (!err?.id) err.id = err.time;//for app
+    if (!err?.time) err.time = new Date().getTime();
+    if (!err?.id) err.id = err.time;
   }
   if (evil || err) throw err;
   if (return_ctx) return [ctxx,rst];
   return rst;
 }
 let jevalx = jevalx_core;
-
-if (typeof module!='undefined') module.exports = {jevalx,jevalx_core,jevalx_raw,S_SETUP,delay,X,findEvil,
+if (typeof module!='undefined') module.exports = {jevalx,jevalx_core,jevalx_raw,S_SETUP,delay,
+X,findEvil,
 VER:'rc3d'
 }
 
--- jevalx_prod.js
+++ jevalx_prod.js
@@ -1,7 +1,11 @@
-Object.defineProperty(Object.prototype, '__proto__', { get() { console.log('host__proto__911_get');return undefined; }, set(newValue) { console.log('host__proto__911_set',newValue) } });
+Object.defineProperty(Object.prototype, '__proto__', {
+  get() { console.log('host__proto__911_get');return undefined; },
+  set(newValue) { console.log('host__proto__911_set',newValue) }
+});
 const X=function(){}
 Object.setPrototypeOf(X.prototype,null);
 Object.setPrototypeOf(X,X.prototype);
+
 Object.defineProperty(globalThis,'AsyncFunction',{value:(async()=>{}).constructor,writable:false,enumerable:false,configurable:false});
 
 eval(['Object.prototype.__defineGetter__','Object.prototype.__defineSetter__'].map(v=>'delete '+v+';').join(''));
@@ -13,23 +17,34 @@ const processWtf = require('process');
 const timers = require('timers');
 const setTimeout = timers.setTimeout;
 const delay = (t,rt)=>new Promise((r,j)=>setTimeout(()=>r(rt),t));
+
 let jevalx_raw = (js,ctxx,timeout=666,js_opts)=>[ctxx,vm.createScript(js,js_opts).runInContext(ctxx,{breakOnSigint:true,timeout})];
+
 const S_SETUP = `(()=>{
-Object.defineProperty(Object.prototype,'__proto__',{ get() { console.log('box__proto__911_get');return undefined; }, set(newValue) { console.log('box__proto__911_set',newValue) } });
+let is_sandbox = 'function'!=typeof setTimeout;
+let Object_defineProperty = Object.defineProperty;
+Object_defineProperty(Object.prototype,'__proto__',{get(){},set(newValue){}});//L0
+if (is_sandbox){
+  let WhiteList = ['Object','Array','JSON','Promise','Function','eval','globalThis','Date','Math'];
+  for (let v of Object.getOwnPropertyNames(this)){
+    if (WhiteList.indexOf(v)<0) delete this[v];
+  };
+};
 `+[
-'WebAssembly','Error','AggregateError','EvalError','RangeError','ReferenceError','SyntaxError','TypeError','URIError',
-'Symbol','Reflect','Proxy','Object.prototype.__defineGetter__','Object.prototype.__defineSetter__','Object.prototype.__lookupSetter__','Object.prototype.__lookupGetter__',
-'FinalizationRegistry',//@r29
+'Object.prototype.__defineGetter__','Object.prototype.__defineSetter__','Object.prototype.__lookupSetter__','Object.prototype.__lookupGetter__',//L0
 ].map(v=>'delete '+v+';').join('')
 +`
-for(let k of Object.getOwnPropertyNames(Object)){if(['name','fromEntries','keys','entries','is','values','getOwnPropertyNames'].indexOf(k)<0){delete Object[k]}}return Promise})()`;
+Object.defineProperty(globalThis,'AsyncFunction',{value:(async()=>{}).constructor,writable:false,enumerable:false,configurable:false});
+//setTimeout = (f,t)=>Promise.delay(t).then(f);
+for(let k of Object.getOwnPropertyNames(Object)){if(['name','fromEntries','keys','entries','is','values','getOwnPropertyNames'].indexOf(k)<0){delete Object[k]}}//L0
+return Promise})()`;
 
 let jevalx_host_a = [ RangeError, TypeError, Promise, Object, AsyncFunction, Function ];
 let jevalx_core = async(js,ctx,options={})=>{
   let {timeout=666,json_output=false,return_ctx=false,user_import_handler=undefined}=(typeof options=='object'?options:{});
   if (typeof options=='number') timeout = options;
   let ctxx,rst,err,evil=0,jss= JSON.stringify(js),done=false;
-  let last_resolve,last_reject;
+  let last_reject;
     let tmpHandler = (ex, promise)=>{ if (!err) err={message:ex?.message||'EvilXa',js,code:ex?.code,tag:Promise?'Xb':'Xa',ex};
       if (!done && last_reject) {
         last_reject(err);
@@ -48,7 +63,7 @@ let jevalx_core = async(js,ctx,options={})=>{
           ctxx = vm.createContext(new X);
           [ctxx,_Promise] = jevalx_raw(S_SETUP,ctxx);
           _Promise.delay = (t,r)=>new _Promise((rr,jj)=>delay(t).then(()=>(done||rr(r))));
-          ctxx.console = console;
+          ctxx.console = {log:console.log};//can be replaced by ctx.console
           if (ctx) Object.assign(ctxx,ctx);
         }
         jevalx_host_a.forEach(o=>(o.prototype.constructor=X));
