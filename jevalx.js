d0903e75f29aefd20c50327d71fda414f66ee600
wjchk <wanjochan@gmail.com>
2024-04-26 10:53:32 +0800

    cleanup and boosting;

--- jevalx.js
+++ jevalx.js
@@ -24,8 +24,6 @@ if (is_sandbox){
 };
 `+['Object.prototype.__defineGetter__','Object.prototype.__defineSetter__','Object.prototype.__lookupSetter__','Object.prototype.__lookupGetter__'].map(v=>'delete '+v+';').join('')
 +`
-//Object_defineProperty(this,'setTimeout',{get(){return (f,t)=>Promise.delay(t).then(f)}});
-//try{Object.setPrototypeOf(this,null);}catch(e){}
 for(let k of Object.getOwnPropertyNames(Object)){if(['name','fromEntries','keys','entries','is','values','getOwnPropertyNames'].indexOf(k)<0){delete Object[k]}}return [console,Promise,Object,Function,globalThis]})()`;
 let jevalx_host_name_a=['Promise','Object','Function'];
 const S_ENTER = jevalx_host_name_a.map(v=>`${v}.prototype.constructor=X;`).join('');//
@@ -36,39 +34,36 @@ const timers = require('timers');
 const setTimeout = timers.setTimeout;
 const delay = (t,rt)=>new Promise((r,j)=>setTimeout(()=>r(rt),t));
 let jevalx_raw = (js,ctxx,timeout=666,js_opts)=>[ctxx,vm.createScript(js,js_opts).runInContext(ctxx,{breakOnSigint:true,timeout})];
+let filterError = (ex,err,jss) =>{
+  if (!err) {
+    let message = 'EvilX', code;
+    if (ex) {
+      Object_setPrototypeOf(ex,Object.prototype);//clear intended __proto__
+      let message_desc = Object_getOwnPropertyDescriptor(ex,'message');//@t4
+      if (message_desc && (message_desc.get || message_desc.set)){
+      } else if(ex.message) { message = ex.message; }
+      let code_desc = Object_getOwnPropertyDescriptor(ex,'code');//@t3
+      if (code_desc && (code_desc.get || code_desc.set)){
+      }else if(ex.code) { code = ex.code }
+      err={message,jss};
+      if (code) err.code = code;
+    }
+  }
+  return err;
+}
 let jevalx_core = async(js,ctx,options={})=>{
   let call_id = 'code'+new Date().getTime();
   let {timeout=666,json_output=false,return_arr=false,user_import_handler=undefined,microtaskMode='afterEvaluate'}=(typeof options=='object'?options:{});
   if (microtaskMode!='afterEvaluate') microtaskMode = undefined;
   if (typeof options=='number') timeout = options;
   let ctxx,rst,err,jss= JSON.stringify(js),last_reject;
-  //TODO filterError moved to outer level for boosting
-  let filterError = (ex) =>{
-    if (!err) {
-      let message = 'EvilX', code;
-      if (ex) {
-        Object_setPrototypeOf(ex,Object.prototype);//clear intended __proto__
-        let message_desc = Object_getOwnPropertyDescriptor(ex,'message');//@t4
-        if (message_desc && (message_desc.get || message_desc.set)){
-        } else if(ex.message) { message = ex.message; }
-        let code_desc = Object_getOwnPropertyDescriptor(ex,'code');//@t3
-        if (code_desc && (code_desc.get || code_desc.set)){
-        }else if(ex.code) { code = ex.code }
-        err={message,jss};
-        if (code) err.code = code;
-      }
-    }
-    return err;
-  }
   try{
     let _console,_Promise,_Object,_Function;
     await new Promise(async(resolve,reject)=>{
       setTimeout(()=>{reject({message:'TimeoutX',code:'ERR_SCRIPT_EXECUTION_TIMEOUT',js})},timeout+111);//Q7x
-      let onError = (ex, tag)=>{ reject(filterError(ex)); };
       if (ctx && vm.isContext(ctx)) ctxx = ctx;
       else {
         ctxx = vm.createContext(new function(){},{microtaskMode});
-        //Object_setPrototypeOf(ctxx,null);
         [ctxx,[_console,_Promise,_Object,_Function]] = jevalx_raw(S_SETUP,ctxx);
         _console.log = console.log;
         _console.error = console.error;
@@ -77,9 +72,9 @@ let jevalx_core = async(js,ctx,options={})=>{
       eval(S_ENTER);
       try{
         jevalx_raw(`(async()=>{try{return await(async(z)=>{while(z&&((z instanceof Promise)&&(z=await z)||(typeof z=='function')&&(z=z())));if(typeof(z)=='object'){z=Array.isArray(z)?[...z]:{...z}}if(z){delete z.then;delete z.toString;delete z.toJSON;delete z.constructor}if(${!!json_output})z=JSON.stringify(z);return z})(eval(${jss}))}catch(ex){return Promise.reject(ex)}})()`,ctxx,timeout,{filename:call_id})[1].then(tmp_rst=>{ delete _Promise.prototype.then;rst=tmp_rst;resolve()}).catch(ex=>{reject(ex)})
-      }catch(ex){ err = filterError(ex); reject(err); }
+      }catch(ex){ err = filterError(ex,err,jss); reject(err); }
     });
-  }catch(ex){ if (!err) { err = filterError(ex) } }
+  }catch(ex){ err = filterError(ex,err,jss) }
   finally{ eval(S_EXIT); }
   if (err) {
     if (err?.code=='ERR_VM_DYNAMIC_IMPORT_CALLBACK_MISSING_FLAG') { err.message = 'EvilImportX'; err.code='EVIL_IMPORT_FLAG';}
